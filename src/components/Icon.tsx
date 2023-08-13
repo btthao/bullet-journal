9a76fcc8688ec56346dd2e76d6dd935fc7943ab5
Thao Bui <thuthao896@gmail.com>
2023-08-13 16:44:48 +0100

    color picker

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -16,6 +16,10 @@ const Icon: React.FC<IconProps> = ({ type }) => {
       Svg = Close;
       break;
 
+    case 'color-picker':
+      Svg = ColorPicker;
+      break;
+
     default:
       throw Error(`Invalid icon type "${type}"`);
   }
@@ -30,16 +34,12 @@ const Edit = () => (
     <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
     <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
     <g id='SVGRepo_iconCarrier'>
-      <path d='M11 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22H15C20 22 22 20 22 15V13' stroke='#292D32' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round'></path>
       <path
-        d='M16.04 3.02001L8.16 10.9C7.86 11.2 7.56 11.79 7.5 12.22L7.07 15.23C6.91 16.32 7.68 17.08 8.77 16.93L11.78 16.5C12.2 16.44 12.79 16.14 13.1 15.84L20.98 7.96001C22.34 6.60001 22.98 5.02001 20.98 3.02001C18.98 1.02001 17.4 1.66001 16.04 3.02001Z'
-        stroke='#292D32'
-        strokeWidth='1.5'
-        strokeMiterlimit='10'
+        d='M14.3601 4.07866L15.2869 3.15178C16.8226 1.61607 19.3125 1.61607 20.8482 3.15178C22.3839 4.68748 22.3839 7.17735 20.8482 8.71306L19.9213 9.63993M14.3601 4.07866C14.3601 4.07866 14.4759 6.04828 16.2138 7.78618C17.9517 9.52407 19.9213 9.63993 19.9213 9.63993M14.3601 4.07866L12 6.43872M19.9213 9.63993L14.6607 14.9006L11.5613 18L11.4001 18.1612C10.8229 18.7383 10.5344 19.0269 10.2162 19.2751C9.84082 19.5679 9.43469 19.8189 9.00498 20.0237C8.6407 20.1973 8.25352 20.3263 7.47918 20.5844L4.19792 21.6782M4.19792 21.6782L3.39584 21.9456C3.01478 22.0726 2.59466 21.9734 2.31063 21.6894C2.0266 21.4053 1.92743 20.9852 2.05445 20.6042L2.32181 19.8021M4.19792 21.6782L2.32181 19.8021M2.32181 19.8021L3.41556 16.5208C3.67368 15.7465 3.80273 15.3593 3.97634 14.995C4.18114 14.5653 4.43213 14.1592 4.7249 13.7838C4.97308 13.4656 5.26166 13.1771 5.83882 12.5999L8.5 9.93872'
+        stroke='currentColor'
+        strokeWidth='2'
         strokeLinecap='round'
-        strokeLinejoin='round'
       ></path>
-      <path d='M14.91 4.1499C15.58 6.5399 17.45 8.4099 19.85 9.0899' stroke='#292D32' strokeWidth='1.5' strokeMiterlimit='10' strokeLinecap='round' strokeLinejoin='round'></path>
     </g>
   </svg>
 );
@@ -49,7 +49,20 @@ const Close = () => (
     <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
     <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
     <g id='SVGRepo_iconCarrier'>
-      <path d='M8 8L40 40' stroke='#000000' strokeWidth='4.8' strokeLinecap='round' strokeLinejoin='round'></path> <path d='M8 40L40 8' stroke='#000000' strokeWidth='4.8' strokeLinecap='round' strokeLinejoin='round'></path>
+      <path d='M8 8L40 40' stroke='currentColor' strokeWidth='5.5' strokeLinecap='round' strokeLinejoin='round'></path> <path d='M8 40L40 8' stroke='currentColor' strokeWidth='5.5' strokeLinecap='round' strokeLinejoin='round'></path>
+    </g>
+  </svg>
+);
+
+const ColorPicker = () => (
+  <svg viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg' fill='currentColor'>
+    <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+    <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
+    <g id='SVGRepo_iconCarrier'>
+      <rect x='0' fill='none' width='20' height='20'></rect>
+      <g>
+        <path d='M17.8 2.2c-1-1-2.6-1-3.6 0L12.4 4l-.7-.7c-.4-.4-1-.4-1.4 0l-.8.7c-.4.4-.4 1 0 1.4l5 5c.4.4 1 .4 1.4 0l.7-.7c.4-.4.4-1 0-1.4l-.6-.7 1.8-1.8c1-1 1-2.6 0-3.6zM4.4 12c-2.2 2.2-.9 3.2-2.9 5.8l.7.7c2.6-2 3.6-.7 5.8-2.9l5.1-5.1-3.6-3.6L4.4 12z'></path>
+      </g>
     </g>
   </svg>
 );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,6 +9,7 @@ import TextInput from './TextInput';
 import Label from './Label';
 import { produce } from 'immer';
 import { set } from 'lodash';
+import { HexColorPicker } from 'react-colorful';
 
 interface MainSectionProps {
   data: LogData;
@@ -60,8 +61,28 @@ interface EditModalProps extends MainSectionProps {
   closeModal: () => void;
 }
 
-const EditModal = ({ data, closeModal, editLog }: EditModalProps) => {
+const EditModal = (props: EditModalProps) => {
+  const { closeModal } = props;
+  return (
+    <div className='fixed top-0 left-0 w-full h-full z-[5000]'>
+      <div className='absolute top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 -z-10' onClick={closeModal}></div>
+      <div className='w-11/12 max-w-[450px] max-h-[90vh] mx-auto mt-[5vh] p-6 rounded-lg bg-white shadow-lg overflow-scroll'>
+        <div className='flex text-xl font-bold mb-4 -mr-2 items-center justify-between'>
+          <h6>Edit log</h6>
+          <button className='w-8 h-8 p-2 hover:bg-neutral-100  rounded-full' onClick={closeModal}>
+            <Icon type='close' />
+          </button>
+        </div>
+        <EditForm {...props} />
+      </div>
+    </div>
+  );
+};
+
+const EditForm = ({ data, editLog, closeModal }: EditModalProps) => {
   const [values, setValues] = useState(data);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [selectedKeyIdx, setSelectedKeyIdx] = useState<number | null>(null);
 
   const handleChange = (name: string, value: string) => {
     setValues(
@@ -109,6 +130,9 @@ const EditModal = ({ data, closeModal, editLog }: EditModalProps) => {
       })
     );
   };
+
+  const validate = () => {
+    // form validation
   };
 
   const handleSubmit = (e: FormEvent) => {
@@ -118,77 +142,99 @@ const EditModal = ({ data, closeModal, editLog }: EditModalProps) => {
   };
 
   return (
-    <div className='fixed top-0 left-0 w-full h-full z-[5000]'>
-      <div className='absolute top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 -z-10' onClick={closeModal}></div>
-      <div className='w-11/12 max-w-[450px] max-h-[90vh] mx-auto mt-[5vh] p-6 rounded-lg bg-white shadow-2xl overflow-scroll'>
-        <div className='flex text-xl font-bold mb-4 items-center justify-between'>
-          <h6>Edit log</h6>
-          <button className='w-8 h-8 p-2 hover:bg-neutral-100  rounded-full' onClick={closeModal}>
-            <Icon type='close' />
-          </button>
+    <form className='grid gap-6 ' onSubmit={handleSubmit}>
+      <div className='flex gap-4'>
+        <div className='relative p-1 grid place-items-center'>
+          <Emoji unified={values.emoji} size={60} emojiStyle={EmojiStyle.NATIVE} />
+          <div role='button' className='absolute w-full h-full top-0 left-0 bg-neutral-900 bg-opacity-40 grid place-items-center rounded-md text-white opacity-0 hover:opacity-100' onClick={() => setShowEmojiPicker((prev) => !prev)}>
+            <div className='w-9'>
+              <Icon type='edit' />
+            </div>
+          </div>
         </div>
-        <form className='grid gap-6 ' onSubmit={handleSubmit}>
-          <div className='flex gap-4'>
-            <div>
-              <Emoji unified={values.emoji} size={60} emojiStyle={EmojiStyle.NATIVE} />
-              <div className='hidden'>
-                <EmojiPicker
-                  emojiStyle={EmojiStyle.NATIVE}
-                  previewConfig={{ showPreview: false }}
-                  suggestedEmojisMode={undefined}
-                  categories={[
-                    { category: Categories.SMILEYS_PEOPLE, name: 'People' },
-                    { category: Categories.ACTIVITIES, name: 'Activities' },
-                    { category: Categories.ANIMALS_NATURE, name: 'Animals & Nature' },
-                    { category: Categories.FLAGS, name: 'Flags' },
-                    { category: Categories.FOOD_DRINK, name: 'Food & Drinks' },
-                    { category: Categories.OBJECTS, name: 'Objects' },
-                    { category: Categories.SYMBOLS, name: 'Symbols' },
-                    { category: Categories.TRAVEL_PLACES, name: 'Places' },
-                  ]}
-                />
+        <div className='flex-1'>
+          <Label>Name</Label>
+          <TextInput value={values.name} name='name' placeholder='Name' handleChange={handleChange} />
+        </div>
+      </div>
+      <div>
+        <Label>Description</Label>
+        <TextInput value={values.description} name='description' placeholder='Description' handleChange={handleChange} />
+      </div>
+      <div>
+        <Label>Keys</Label>
+        <button type='button' className='mt-1 mb-3 px-2 py-1 rounded-md text-sm font-medium bg-neutral-200' onClick={addNewKey}>
+          + New key
+        </button>
+        <div className='grid gap-2'>
+          {values.keys.map((key, idx) => (
+            <div key={key.value} className='flex relative rounded-md  min-w-fit items-center text-sm leading-4'>
+              <span
+                className='w-5 h-5  inline-block flex-shrink-0 absolute left-2.5 rounded-md'
+                style={{
+                  background: key.color,
+                }}
+              ></span>
+              <TextInput value={key.label} name={`keys[${idx}].label`} placeholder='label' className='pl-10 pr-14' handleChange={handleChange} />
+              <div className='flex-shrink-0 absolute right-1 text-neutral-500'>
+                <button type='button' className='w-6 h-6 p-1 rounded-full hover:bg-neutral-200' onClick={() => setSelectedKeyIdx(idx)}>
+                  <Icon type='color-picker' />
+                </button>
+                {idx >= data.keys.length && (
+                  <button type='button' className='w-6 h-6 p-1 rounded-full hover:bg-neutral-200' onClick={() => deleteKey(key.value)}>
+                    <Icon type='close' />
+                  </button>
+                )}
               </div>
             </div>
-            <div className='flex-1'>
-              <Label>Name</Label>
-              <TextInput value={values.name} name='name' placeholder='Name' handleChange={handleChange} />
-            </div>
-          </div>
-          <div>
-            <Label>Description</Label>
-            <TextInput value={values.description} name='description' placeholder='Description' handleChange={handleChange} />
-          </div>
-          <div>
-            <Label>Keys</Label>
-            <button type='button' className='mt-1 mb-3 px-2 py-1 rounded-md text-sm font-medium bg-neutral-200' onClick={addNewKey}>
-              + New key
-            </button>
-            <div className='grid grid-cols-2 gap-2'>
-              {values.keys.map((key, idx) => (
-                <div key={key.value} className='flex relative rounded-md  min-w-fit items-center text-sm leading-4'>
-                  <span
-                    className='w-5 h-5  inline-block flex-shrink-0 absolute left-2.5 rounded-md'
-                    style={{
-                      background: key.color,
-                    }}
-                  ></span>
-                  <TextInput value={key.label} name={`keys[${idx}].label`} placeholder='label' className='pl-10 pr-8' handleChange={handleChange} />
-                  {idx >= data.keys.length && (
-                    <button type='button' className='w-5 h-5 p-1 flex-shrink-0 absolute right-2 rounded-full hover:bg-neutral-200' onClick={() => deleteKey(key.value)}>
-                      <Icon type='close' />
-                    </button>
-                  )}
-                </div>
-              ))}
-            </div>
-          </div>
-          <div>
-            <button type='submit' className='text-white bg-gray-900  font-bold rounded-full w-auto px-8 py-3 text-center float-right'>
-              Save
+          ))}
+        </div>
+        {showEmojiPicker && (
+          <div className='fixed bottom-0 left-1 z-50'>
+            <EmojiPicker
+              width={280}
+              height={340}
+              emojiStyle={EmojiStyle.NATIVE}
+              previewConfig={{ showPreview: false }}
+              suggestedEmojisMode={undefined}
+              onEmojiClick={(emoji) => {
+                handleChange('emoji', emoji.unified);
+              }}
+              categories={[
+                { category: Categories.SMILEYS_PEOPLE, name: 'People' },
+                { category: Categories.ACTIVITIES, name: 'Activities' },
+                { category: Categories.ANIMALS_NATURE, name: 'Animals & Nature' },
+                { category: Categories.FLAGS, name: 'Flags' },
+                { category: Categories.FOOD_DRINK, name: 'Food & Drinks' },
+                { category: Categories.OBJECTS, name: 'Objects' },
+                { category: Categories.SYMBOLS, name: 'Symbols' },
+                { category: Categories.TRAVEL_PLACES, name: 'Places' },
+              ]}
+            />
+            <button className='absolute top-1 right-1 small-close-btn bg-neutral-200 z-10' onClick={() => setShowEmojiPicker(false)}>
+              <Icon type='close' />
             </button>
           </div>
-        </form>
+        )}
+        {selectedKeyIdx !== null && (
+          <div className='fixed bottom-0 right-1 z-50 color-picker-wrapper pt-6 p-3'>
+            <HexColorPicker
+              color={values.keys[selectedKeyIdx].color}
+              onChange={(newColor) => {
+                handleChange(`keys[${selectedKeyIdx}].color`, newColor);
+              }}
+            />
+            <button className='absolute top-1 right-1 small-close-btn bg-neutral-200 z-10' onClick={() => setSelectedKeyIdx(null)}>
+              <Icon type='close' />
+            </button>
+          </div>
+        )}
       </div>
-    </div>
+      <div>
+        <button type='submit' className='text-white bg-gray-900  font-bold rounded-full w-auto px-8 py-3 text-center float-right'>
+          Save
+        </button>
+      </div>
+    </form>
   );
 };
